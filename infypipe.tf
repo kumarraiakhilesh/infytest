5b43b519a4c1e196db18c985ceecb21d6cef9fee
kumarraiakhilesh@gmail.com <kumarraialhiesh@gmail.com>
2026-04-24 22:53:44 +0530

    infra code

---
+++ infypipe.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.70.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "matru"
+    storage_account_name="mlki"
+    container_name="mogli"
+    key="int.tfstste"
+}
+    }
+provider "azurerm"{
+    features{}
+    subscription_id=" 9097d0f8-4798-48d3-891f-83da77060e96"
+}
+resource "azurerm_resource_group" "rgifny" {
+    name="matru"
+  location="uksouth"
+}
