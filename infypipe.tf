5bef24bb81f57d0fdba452f604d9b861e4de494d
kumarraiakhilesh <kumarraiakhilesh@gmail.com>
2026-04-25 06:31:58 +0530

    Update infypipe.tf

--- infypipe.tf
+++ infypipe.tf
@@ -14,7 +14,7 @@ terraform {
     }
 provider "azurerm"{
     features{}
-    subscription_id=" 9097d0f8-4798-48d3-891f-83da77060e96"
+    subscription_id="9097d0f8-4798-48d3-891f-83da77060e96"
 }
 resource "azurerm_resource_group" "rgifny" {
     name="matru"
