c1af624c2fdb7328f37eb11c27495ccd4e793a90
kumarraiakhilesh <kumarraiakhilesh@gmail.com>
2026-04-25 07:22:14 +0530

    Update infypipe.tf

--- infypipe.tf
+++ infypipe.tf
@@ -12,14 +12,14 @@ provider "azurerm" {
   subscription_id = "9097d0f8-4798-48d3-891f-83da77060e96"
 }
 
-resource "azurerm_resource_group" "rgkaka" {
-  name     = "matab"
+resource "azurerm_resource_group" "rgkaka1" {
+  name     = "matab1"
   location = "uksouth"
 }
 
 resource "azurerm_storage_account" "dyiniya" {
   name                     = "maihuna12345"  # unique hona chahiye
-  resource_group_name      = azurerm_resource_group.rgkaka.name
+  resource_group_name      = azurerm_resource_group.rgkaka1.name
   location                 = azurerm_resource_group.rgkaka.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
