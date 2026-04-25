897c7dd0e2ec5fec7419c9f5872f223d5b355911
kumarraiakhilesh <kumarraiakhilesh@gmail.com>
2026-04-25 07:23:22 +0530

    Update infypipe.tf

--- infypipe.tf
+++ infypipe.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "rgkaka1" {
 resource "azurerm_storage_account" "dyiniya" {
   name                     = "maihuna12345"  # unique hona chahiye
   resource_group_name      = azurerm_resource_group.rgkaka1.name
-  location                 = azurerm_resource_group.rgkaka.location
+  location                 = azurerm_resource_group.rgkaka1.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
